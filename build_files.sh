2121b36d46855debe94acde19c51ccd4f5bf72b8
Thon <137491012+thon1525@users.noreply.github.com>
2025-04-08 15:53:02 +0700

    Fix build_files.sh with explicit settings module

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-
 #!/bin/bash
-pip3 install -r requirements.txt
+export DJANGO_SETTINGS_MODULE="wims.settings"
+pip3 install -r requirements.txt  # Optional if @vercel/python handles it
 python3 manage.py collectstatic --noinput
